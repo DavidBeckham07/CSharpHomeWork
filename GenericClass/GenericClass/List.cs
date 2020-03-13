4ce94aef6b7ad7aeda4788a586f60da48d6c4bfa
DavidBackham <1183469020@qq.com>
2020-03-13 18:50:04 +0800

    generic class homework

---
+++ GenericClass/GenericClass/List.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericClass
+{
+    class List<T>
+    {
+        
+        public List<T> next;
+        public T Data { get; set; }
+
+        public List(T t)
+        {
+            next = null;
+            Data = t;
+        }
+
+
+        public void add(T t)
+        {
+            List<T> current = this;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            List<T> next = new List<T>(t);
+            current.next = next;
+        }
+
+        internal void ForEach( Action<T> action)
+        {
+            List<T> current = this;
+            while(current != null)
+            {
+                action(current.Data);
+                current = current.next;
+            }
+        }
+
+ 
+
+
+    }
+
+}
---
+++ GenericClass/GenericClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GenericClass
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>(10);
+            Random r = new Random();
+            for (int i = 1; i < 10; i++)
+            {
+                intList.add(r.Next(0,100)) ;
+                //Console.WriteLine($"No{i}：");
+            }
+
+            intList.ForEach((int x) => Console.WriteLine($"{x}"));
+
+            int sum = 0,max = int.MinValue, min = int.MaxValue;
+
+            intList.ForEach(x => sum += x);
+            intList.ForEach(x => { if (max < x) { max = x; } }) ;
+            intList.ForEach(x => { if (min > x) { min = x; } });
+            Console.WriteLine($"sum:{sum} max:{max} min:{min}");
+
+
+        }
+    }
+}
