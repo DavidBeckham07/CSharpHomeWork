ec386e41c97875bae8b3fcbeb5703f01816f92a0
DavidBackham <1183469020@qq.com>
2020-05-26 19:48:47 +0800

    modify

---
+++ Shape/Shape/IShape.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Shape
+{
+    public interface IShape
+    {
+        double Area { get; }
+    }
+
+    public class Triangle : IShape
+    {
+        public int  Width { get; set; }
+        public int Height { get; set; }
+        public double Area => Width * Height * 0.5;
+        public Triangle(int w, int h)
+        {
+            Width = w;
+            Height = h;
+        }
+
+        public override string ToString()
+        {
+            return $"Triangle(width:{Width}, height{Height}, area{Area})";
+        }
+    }
+
+    public class Rectangle: IShape
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public double Area => Height * Width;
+        public Rectangle(int h, int w)
+        {
+            Height = h;
+            Width = w;
+        }
+        public override string ToString()
+        {
+            return $"Rectangle(Height:{Height}, Width:{Width}, Area:{Area})";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public Square(int width): base(width, width){}
+        public override string ToString()
+        {
+            return $"Square(Width:{Width}, Area:{Area})";
+        }
+
+    }
+}
---
+++ Shape/Shape/ShapeFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shape
+{
+    class ShapeFactory
+    {
+        static Random r = new Random();
+        public static void Calculate(IShape shape)
+        {
+            Console.Out.WriteLine(shape);
+        }
+
+
+        public static void run()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Out.Write(i + ": ");
+                switch(r.Next() % 3)
+                {
+                    case 0:
+                        Calculate(new Rectangle(r.Next(1, 10), r.Next(1, 10)));
+                        break;
+                    case 1:
+                        Calculate(new Square(r.Next(1, 10)));
+                        break;
+                    case 2:
+                        Calculate(new Triangle(r.Next(1, 10), r.Next(1, 10)));
+                        break;
+                }
+ 
+            }
+        }
+    }
+}
