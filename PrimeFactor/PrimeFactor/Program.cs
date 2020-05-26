764d03ffe3338e7f6e50a62f1b94db1ab66589af
DavidBackham <1183469020@qq.com>
2020-05-26 19:00:02 +0800

    prime factor

---
+++ PrimeFactor/PrimeFactor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PrimeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //string result = "";
+            Console.WriteLine("input a positive number");
+            if(int.TryParse(Console.ReadLine(), out int input))
+            {
+                if(input < 0)
+                {
+                    Console.WriteLine("it is not a positive number");
+                }
+                int prime = 2;
+                while(input != 1)
+                {
+                    if(input % prime == 0)
+                    {
+                        input /= prime;
+                        Console.Write(prime.ToString() + " ");
+                    }
+                    else
+                    {
+                        prime++;
+                    }
+
+                }        
+            }
+            else
+            {
+                Console.WriteLine("input error");
+            }
+
+
+
+
+
+        }
+    }
+}
